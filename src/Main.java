db9d3a8f348d7fb44ae174cae5fd2649a60987a2
kumaran26 <kumaran26skr@gmail.com>
2019-10-27 02:22:31 +0200

    feat: Tested negative scenarios for printing in console

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello world !");
+        System.out.println("Hello world !"); // Print and then add new line after that
+        System.out.print("Hello"); // Print and no new line after that
+        //system.out.print("Hello"); // Negative scenario : java package system does not exist
     }
 }
